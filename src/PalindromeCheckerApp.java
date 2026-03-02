b48f96efa98a03e98170b93fbd6d17beea2feed0
Aravind837 <ak8397@srmist.edu.in>
2026-03-02 15:57:38 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,6 +1,3 @@
-public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        System.out.println("welcome to the palindrome checker management system\nVersion : 1.0\nsystem intialized successfully.");
-    }
+public class PalindromeCheckerApp {  
 }
 
