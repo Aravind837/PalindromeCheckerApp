a4ba3fcb18c2da2a2a00ad51415130ab5bcc2e95
Aravind837 <ak8397@srmist.edu.in>
2026-03-02 15:52:15 +0530

    UC1:Add welcome message module

--- src/UseCase1PalindromeApp.java
+++ src/UseCase1PalindromeApp.java
@@ -1,6 +1,5 @@
-public class PalindromeCheckerApp {
+public class UseCase1PalindromeApp {
     public static void main(String[] args) {
         System.out.println("welcome to the palindrome checker management system\nVersion : 1.0\nsystem intialized successfully.");
     }
 }
-
